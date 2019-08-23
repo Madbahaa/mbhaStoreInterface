3720c5294f38a71948a515c8953442ede5015620
Madbahaa <madbahaa@gmail.com>
2019-08-23 12:16:05 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+/**
+ * mbha Store interface
+ */
+
+
+ jQuery(document).ready(function(){
+    var html = '<div class="moo-row">'+
+                '<p>hello there</p>'+
+               '</div>';
+    jQuery('#moo_OnlineStoreContainer').html(html);
+ })
+
+ function mbha_loadBaseStructure(){
+
+ }
